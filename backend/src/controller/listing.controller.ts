6731594db9f43370434a6669d4c409b305ae0f01
Gaurav Deep <gauravdeep8660@gmail.com>
2025-01-11 01:25:24 +0530

    vendor in donation and discount

--- backend/src/controller/listing.controller.ts
+++ backend/src/controller/listing.controller.ts
@@ -3,6 +3,9 @@ import ItemListing from "../models/ItemsListing";
 import { Request, Response } from "express";
 import { IItemListing } from "../models/ItemsListing";
 import { CustomRequest } from "../types/CustomeRequest";
+import { error } from "console";
+
+const DaysBeforeExpiration = 4;
 
 const addVendorListing = async (
   req: CustomRequest,
@@ -13,9 +16,10 @@ const addVendorListing = async (
     const { items, bulkExpirationDate } = req.body;
 
     if (!items || !bulkExpirationDate) {
-      res
-        .status(400)
-        .json({ message: "Items and bulkExpirationDate are required." });
+      res.status(400).json({
+        error: false,
+        message: "Items and bulkExpirationDate are required.",
+      });
       return;
     }
     const bulkExpDate: Date = new Date(bulkExpirationDate);
@@ -28,15 +32,16 @@ const addVendorListing = async (
     const savedListing = await newListing.save();
 
     res.status(201).json({
-      error: true,
+      error: false,
       message: "Listings created",
       data: savedListing,
     });
   } catch (error) {
     console.error(error);
-    res
-      .status(500)
-      .json({ message: "Server error, could not create listing." });
+    res.status(500).json({
+      error: true,
+      message: "Server error, could not create listing.",
+    });
   }
 };
 
@@ -46,25 +51,25 @@ const listDiscountedItems = async (
 ): Promise<void> => {
   try {
     const currentDate = new Date();
+    const fourDaysLater = new Date(
+      currentDate.getTime() + DaysBeforeExpiration * 24 * 60 * 60 * 1000
+    );
 
     const listings: IItemListing[] = await ItemListing.find({
-      bulkExpirationDate: { $gte: currentDate },
+      bulkExpirationDate: { $gte: fourDaysLater },
     });
-
-    const filteredListings = listings.map((listing) => ({
-      ...listing.toObject(),
-      items: listing.items.filter(
-        (item) =>
-          new Date(item.expirationDate) < new Date(listing.bulkExpirationDate)
-      ),
-    }));
-
-    res.status(200).json(filteredListings);
+    console.log("Discount Listing:");
+    console.log(listings);
+    res.status(200).json({
+      error: false,
+      listings,
+    });
   } catch (error) {
     console.error(error);
-    res
-      .status(500)
-      .json({ message: "Server error, could not fetch listings." });
+    res.status(500).json({
+      error: true,
+      message: "Server error, could not fetch listings.",
+    });
   }
 };
 
@@ -78,13 +83,14 @@ const listDonationItems = async (_: Request, res: Response): Promise<void> => {
         $lte: new Date(currentDate.getTime() + 4 * 24 * 60 * 60 * 1000),
       },
     });
-
+    console.log("Donation Listing:");
     res.status(200).json(listings);
   } catch (error) {
     console.error(error);
-    res
-      .status(500)
-      .json({ message: "Server error, could not fetch listings." });
+    res.status(500).json({
+      error: true,
+      message: "Server error, could not fetch listings.",
+    });
   }
 };
 
